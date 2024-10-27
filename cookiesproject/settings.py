c58c11e660ca5166cf4a5062a9dc7cd144cce5e8
suriya04041997 <suriyacse97@gmail.com>
2024-10-27 12:45:27 +0530

    add requirements.txt

--- cookiesproject/settings.py
+++ cookiesproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +26,7 @@ SECRET_KEY = 'django-insecure-m8m%vbabn2%441&*tob&y17ks@66k4d(_^yv$&$pmch)@@$9pg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOST=["*"]
 
 # Application definition
 
@@ -37,11 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp'
+    'myapp.apps.MyappConfig'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -71,14 +72,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cookiesproject.wsgi.application'
 
 
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':dj_database_url.parse("postgresql://mydb_qbbr_user:y1WNeJQgjjBHDvDWmKFC3fL8OeBvtGwu@dpg-cseuei8gph6c73etrm80-a.oregon-postgres.render.com/mydb_qbbr")
 }
 
 
